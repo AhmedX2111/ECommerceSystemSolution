e2e087bb2f3e6d34ff05f54be8ca433b4fef5ceb
AhmedX2111 <Ahmedkhaled910000@gmail.com>
2025-07-04 05:40:21 +0300

    Application Layer

---
+++ Application/Services/CheckoutService.cs
@@ -0,0 +1,69 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+	public class CheckoutService
+	{
+		private readonly IShippingService _shippingService;
+		public CheckoutService(IShippingService shippingService)
+		{
+			_shippingService = shippingService;
+		}
+
+		public void Checkout(Customer customer, Cart cart)
+		{
+			if (!cart.Items.Any())
+				throw new Exception("Cart is empty.");
+
+			decimal subtotal = 0;
+			double totalWeight = 0;
+			var shippableItems = new List<IShippable>();
+
+			foreach (var item in cart.Items)
+			{
+				if (item.Product is IExpirable expirable && expirable.IsExpired())
+					throw new Exception($"{item.Product.Name} is expired.");
+				if (item.Quantity > item.Product.Quantity)
+					throw new Exception($"{item.Product.Name} is out of stock.");
+				subtotal += item.Product.Price * item.Quantity;
+				if (item.Product is IShippable shippable)
+				{
+					shippableItems.Add(shippable);
+					totalWeight += shippable.GetWeight() * item.Quantity;
+				}
+			}
+
+			decimal shippingFee = totalWeight > 0 ? 30 : 0;
+			decimal total = subtotal + shippingFee;
+
+			if (customer.Balance < total)
+				throw new Exception("Insufficient balance.");
+
+			// Deduct quantities and balance
+			foreach (var item in cart.Items)
+				item.Product.Quantity -= item.Quantity;
+			customer.Balance -= total;
+
+			// Ship items
+			if (shippableItems.Any())
+				_shippingService.Ship(shippableItems);
+
+			// Print receipt
+			Console.WriteLine("** Checkout receipt **");
+			foreach (var item in cart.Items)
+				Console.WriteLine($"{item.Quantity}x {item.Product.Name} {item.Product.Price * item.Quantity}");
+			Console.WriteLine("----------------------");
+			Console.WriteLine($"Subtotal {subtotal}");
+			Console.WriteLine($"Shipping {shippingFee}");
+			Console.WriteLine($"Amount {total}");
+			Console.WriteLine($"Customer balance {customer.Balance}");
+		}
+	}
+}
---
+++ Application/Services/ShippingService.cs
@@ -0,0 +1,25 @@
+﻿using Application.Interfaces;
+using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+	public class ShippingService : IShippingService
+	{
+		public void Ship(List<IShippable> items)
+		{
+			Console.WriteLine("** Shipment notice **");
+			double totalWeight = 0;
+			foreach (var item in items)
+			{
+				Console.WriteLine($"{item.GetName()} {item.GetWeight()}kg");
+				totalWeight += item.GetWeight();
+			}
+			Console.WriteLine($"Total package weight {totalWeight}kg");
+		}
+	}
+}
